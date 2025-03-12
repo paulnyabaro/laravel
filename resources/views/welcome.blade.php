1385431a7aaac723ebecc0688c48d9cfe509c61c
paulnyabaro <paulnyabaro1@gmail.com>
2025-03-12 23:26:57 +0300

    Updated auth forms

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+            @vite(['resources/scss/app.scss', 'resources/js/app.js'])
         @else
             <style>
                 
