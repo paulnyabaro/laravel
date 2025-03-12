cf94900567c55a094a901c3fdbdaf80e60a6cfb9
paulnyabaro <paulnyabaro1@gmail.com>
2025-03-12 23:49:13 +0300

    Working on auth forms

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <!-- <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+    </div> -->
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
