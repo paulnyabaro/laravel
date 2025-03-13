f4d87326337f8b41a256f3d9e2c55430aff8afe8
paulnyabaro <paulnyabaro1@gmail.com>
2025-03-13 14:43:00 +0300

    Created blog model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\BlogFactory> */
+    use HasFactory;
 }
